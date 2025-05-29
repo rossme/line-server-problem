8d7d9cad48fd482f6ef2d8aa6868131dc1005ea6
Ross Buddie <r.buddie@gmail.com>
2025-05-29 09:40:10 +0100

    [LSP] update config for cloudflare

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,5 @@
 require "active_support/core_ext/integer/time"
 
-binding.break
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
