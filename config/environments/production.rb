30e3932a70218e363784554ffbd7d114731e57ea
Ross Buddie <r.buddie@gmail.com>
2025-05-29 09:39:43 +0100

    [LSP] update config for cloudflare

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,4 +84,5 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.hosts << config.hosts << /[a-z0-9]+\.run\.foo/
 end
